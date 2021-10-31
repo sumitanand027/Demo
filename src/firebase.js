7bc556f0b96d301f5154c3d47a68ff240c1958f5
sumitanand027 <62176091+sumitanand027@users.noreply.github.com>
2021-10-31 12:04:51 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,20 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-
-  apiKey: "AIzaSyCE0QCJbpyMg8U-yDm5V0lxtWyxvIqeLks",
-
-  authDomain: "event-app-fb8ab.firebaseapp.com",
-
-  projectId: "event-app-fb8ab",
-
-  storageBucket: "event-app-fb8ab.appspot.com",
-
-  messagingSenderId: "152331639497",
-
-  appId: "1:152331639497:web:efd555086b523fe5c60b18"
-
-};
+  //
+};  
 
 
 // Initialize Firebase
